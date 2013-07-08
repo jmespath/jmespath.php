b63d62679d58c54ddd61d7cda538e2f403fda968
Michael Dowling <mtdowling@gmail.com>
2013-07-07 19:31:16 -0700

    Restructuring compliance test case

--- tests/JamesPath/Tests/ComplianceTest.php
+++ tests/JamesPath/Tests/ComplianceTest.php
@@ -7,62 +7,14 @@ use JamesPath\Parser;
 
 class ComplianceTest extends \PHPUnit_Framework_TestCase
 {
-    public function basicTests()
-    {
-        return $this->loadTestCases('basic');
-    }
-
     /**
-     * @dataProvider basicTests
+     * @dataProvider complianceProvider
      */
-    public function testPassesBasic($data, $expression, $result)
-    {
-        $this->doTest($data, $expression, $result);
-    }
-
-    public function indicesTests()
-    {
-        return $this->loadTestCases('indices');
-    }
-
-    /**
-     * @dataProvider indicesTests
-     */
-    public function testPassesIndices($data, $expression, $result)
-    {
-        $this->doTest($data, $expression, $result);
-    }
-
-    public function orMatchTests()
-    {
-        return $this->loadTestCases('ormatch');
-    }
-
-    /**
-     * @dataProvider orMatchTests
-     */
-    public function testPassesOrMatch($data, $expression, $result)
+    public function testPassesCompliance($data, $expression, $result)
     {
         // Fix the old "or" syntax
         $expression = str_replace(' or ', ' || ', $expression);
-        $this->doTest($data, $expression, $result);
-    }
 
-    public function wildcardTests()
-    {
-        return $this->loadTestCases('wildcard');
-    }
-
-    /**
-     * @dataProvider wildcardTests
-     */
-    public function testPassesWildcard($data, $expression, $result)
-    {
-        $this->doTest($data, $expression, $result);
-    }
-
-    protected function doTest($data, $expression, $result)
-    {
         $ast = Parser::compile($expression);
         $parsed = Parser::search($expression, $data);
         $this->assertEquals(
@@ -72,15 +24,17 @@ class ComplianceTest extends \PHPUnit_Framework_TestCase
         );
     }
 
-    protected function loadTestCases($name)
+    public function complianceProvider()
     {
-        $contents = file_get_contents(__DIR__ . '/../../../vendor/boto/jmespath/tests/compliance/' . $name . '.json');
-        $json = json_decode($contents, true);
         $cases = array();
 
-        foreach ($json as $suite) {
-            foreach ($suite['cases'] as $case) {
-                $cases[] = array($suite['given'], $case['expression'], $case['result']);
+        foreach (array('basic', 'indices', 'ormatch', 'wildcard') as $name) {
+            $contents = file_get_contents(__DIR__ . "/../../../vendor/boto/jmespath/tests/compliance/{$name}.json");
+            $json = json_decode($contents, true);
+            foreach ($json as $suite) {
+                foreach ($suite['cases'] as $case) {
+                    $cases[] = array($suite['given'], $case['expression'], $case['result']);
+                }
             }
         }
 
