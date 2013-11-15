868fcf0007997d996639de0874687d2aeb0430ea
Michael Dowling <mtdowling@gmail.com>
2013-11-14 19:00:12 -0800

    Adding timing to jp.php

--- jp.php
+++ jp.php
@@ -14,7 +14,16 @@ echo "Data:\n-----\n\n" . json_encode($data, JSON_PRETTY_PRINT) . "\n\n";
 
 $parser = new Parser(new Lexer());
 $interpreter = new Interpreter(true);
+
+$t = microtime(true);
 $opcodes = $parser->compile($expression);
+$parseTime = (microtime(true) - $t) * 1000;
+
+$t = microtime(true);
 $result = $interpreter->execute($opcodes, $data);
+$interpretTime = (microtime(true) - $t) * 1000;
 
 echo sprintf("Result:\n-------\n\n%s\n\n", json_encode($result, JSON_PRETTY_PRINT));
+echo "Parse time:     {$parseTime} ms\n";
+echo "Interpret time: {$interpretTime} ms\n";
+echo "Total time:     " . ($parseTime + $interpretTime) . " ms\n";
