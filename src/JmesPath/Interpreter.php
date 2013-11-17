066e734265a0d22ac2d8d5548647657871ac8e66
Michael Dowling <mtdowling@gmail.com>
2013-11-16 22:17:27 -0800

    Moving to switch

--- src/JmesPath/Interpreter.php
+++ src/JmesPath/Interpreter.php
@@ -10,15 +10,6 @@ class Interpreter
     /** @var array Array of known opcodes */
     private $methods;
 
-    /** @var array */
-    private $stack;
-
-    /** @var \ArrayIterator */
-    private $i;
-
-    /** @var array */
-    private $eaches;
-
     /** @var bool */
     private $debug;
 
@@ -39,237 +30,149 @@ class Interpreter
      */
     public function execute(array $opcodes, array $data)
     {
-        $this->i = new \ArrayIterator($opcodes);
-        $this->stack = [&$data, $data];
-        $this->eaches = [];
+        $iter = new \ArrayIterator($opcodes);
+        $stack = [&$data, &$data];
+        $eaches = [];
 
         if ($this->debug) {
             $this->debugInit($opcodes, $data);
         }
 
-        while ($this->i->valid()) {
+        while ($iter->valid()) {
 
-            $op = $this->i->current();
-            $arg = 'op_' . $op[0];
+            $opArray = $iter->current();
+            $op = $opArray[0];
+            $arg = isset($opArray[1]) ? $opArray[1] : null;
 
             if ($this->debug) {
-                $this->debugLine($op);
+                $this->debugLine($iter->key(), $stack, $opArray);
             }
 
-            if (!isset($this->methods[$arg])) {
-                throw new \RuntimeException('Unknown opcode {$op}');
-            }
-            $this->{$arg}(isset($op[1]) ? $op[1] : null);
+            switch ($op) {
 
-            if ($arg != 'op_goto') {
-                $this->i->next();
-            }
-        }
+                case 'push':
+                    $stack[] = $arg;
+                    break;
 
-        $this->eaches = $this->i = null;
+                case 'pop':
+                    array_pop($stack);
+                    break;
 
-        return array_pop($this->stack);
-    }
+                case 'dup_top':
+                    $tos = array_pop($stack);
+                    $stack[] = $tos;
+                    $stack[] =& $tos;
+                    break;
 
-    /**
-     * Push a variable into the VM state operand stack
-     *
-     * @param string $arg Value to push
-     */
-    public function op_push($arg = null)
-    {
-        $this->stack[] = $arg;
-    }
+                case 'field':
+                    $tos = array_pop($stack);
+                    $stack[] = is_array($tos) && isset($tos[$arg]) ? $tos[$arg] : null;
+                    break;
 
-    /**
-     * Descends into a specific index of the input data
-     *
-     * @param mixed $arg
-     */
-    private function op_index($arg = null)
-    {
-        $tos = array_pop($this->stack);
-        if (!is_array($tos)) {
-            $this->stack[] = null;
-        } else {
-            $arg = $arg < 0 ? count($tos) + $arg : $arg;
-            $this->stack[] = isset($tos[$arg]) ? $tos[$arg] : null;
-        }
-    }
+                case 'index':
+                    $tos = array_pop($stack);
+                    if (!is_array($tos)) {
+                        $stack[] = null;
+                    } else {
+                        $arg = $arg < 0 ? count($tos) + $arg : $arg;
+                        $stack[] = isset($tos[$arg]) ? $tos[$arg] : null;
+                    }
+                    break;
 
-    /**
-     * Descends into a specific key
-     *
-     * @param mixed $arg
-     */
-    private function op_field($arg = null)
-    {
-        $tos = array_pop($this->stack);
-        $this->stack[] = is_array($tos) && isset($tos[$arg]) ? $tos[$arg] : null;
-    }
+                case 'rot_two':
+                    $tos = array_pop($stack);
+                    $tos1 = array_pop($stack);
+                    $stack[] = $tos;
+                    $stack[] = $tos1;
+                    break;
 
-    /**
-     * Duplicates the top item on the stack by reference
-     */
-    private function op_dup_top()
-    {
-        $tos = array_pop($this->stack);
-        $this->stack[] = $tos;
-        $this->stack[] =& $tos;
-    }
+                case 'rot_three':
+                    $tos = array_pop($stack);
+                    $tos1 = array_pop($stack);
+                    $tos2 = array_pop($stack);
+                    $stack[] = $tos;
+                    $stack[] = $tos2;
+                    $stack[] = $tos1;
+                    break;
 
-    /**
-     * Pops the top item off of the stack
-     */
-    private function op_pop()
-    {
-        array_pop($this->stack);
-    }
+                case 'jump_if_true':
+                    if (end($stack)) {
+                        $iter->seek($arg);
+                    }
+                    break;
 
-    /**
-     * Swaps the top two items on the stack
-     */
-    private function op_rot_two()
-    {
-        $tos = array_pop($this->stack);
-        $tos1 = array_pop($this->stack);
-        $this->stack[] = $tos;
-        $this->stack[] = $tos1;
-    }
+                case 'jump_if_false':
+                    $tos = end($stack);
+                    if ($tos === null || $tos === []) {
+                        $iter->seek($arg);
+                    }
+                    break;
 
-    /**
-     * Lifts second and third stack item one position up, moves top down to
-     * position three.
-     */
-    private function op_rot_three()
-    {
-        $tos = array_pop($this->stack);
-        $tos1 = array_pop($this->stack);
-        $tos2 = array_pop($this->stack);
-        $this->stack[] = $tos;
-        $this->stack[] = $tos2;
-        $this->stack[] = $tos1;
-    }
+                case 'goto':
+                    $iter->seek($arg);
+                    continue 2;
 
-    /**
-     * Jump to the given bytecode index if TOS is true. Leaves TOS on the stack
-     * if true. Pops TOS if false.
-     *
-     * @param int $arg Bytecode index to jump to
-     */
-    private function op_jump_if_true($arg)
-    {
-        if (end($this->stack)) {
-            $this->i->seek($arg);
-        }
-    }
+                case 'store_key':
+                    $tos = array_pop($stack);
+                    $tos1 = array_pop($stack);
 
-    /**
-     * Jump to the given bytecode index if TOS is null or an empty array. Leaves
-     * TOS on the stack.
-     *
-     * @param int $arg Bytecode index to jump to
-     */
-    private function op_jump_if_false($arg)
-    {
-        $tos = end($this->stack);
-        if ($tos === null || $tos === []) {
-            $this->i->seek($arg);
-        }
-    }
+                    if (!is_array($tos1)) {
+                        $stack[] = [];
+                    } else {
+                        if ($arg === null) {
+                            $tos1[] = $tos;
+                        } else {
+                            $tos1[$arg] = $tos;
+                        }
+                        $stack[] = $tos1;
+                    }
+                    break;
 
-    /**
-     * Jumps to the given bytecode index
-     *
-     * @param int $arg Bytecode index to jump to
-     */
-    private function op_goto($arg)
-    {
-        $this->i->seek($arg);
-    }
+                case 'each':
+                    $index = $iter->key();
+                    $tos = array_pop($stack);
 
-    /**
-     * Halts execution
-     */
-    private function op_stop() {}
+                    if (isset($eaches[$index])) {
 
-    /**
-     * Pops two items off of the stack, TOS and TOS1. Then pushes TOS1 back on
-     * the stack after setting TOS1[$arg] = TOS. If no operand, $arg, is
-     * provided, the TOS is appended to TOS1.
-     *
-     * @param string $arg Index to set
-     */
-    private function op_store_key($arg = null)
-    {
-        $tos = array_pop($this->stack);
-        $tos1 = array_pop($this->stack);
+                        list($eachIter, $jmp,) = $eaches[$index];
+                        // Add to the results if not empty
+                        if (!empty($tos)) {
+                            $eaches[$index][2][] = $tos;
+                        }
 
-        if (!is_array($tos1)) {
-            $this->stack[] = [];
-        } else {
-            if ($arg === null) {
-                $tos1[] = $tos;
-            } else {
-                $tos1[$arg] = $tos;
-            }
-            $this->stack[] = $tos1;
-        }
-    }
+                        $eachIter->next();
+                        if ($eachIter->valid()) {
+                            $stack[] = $eachIter->current();
+                        } else {
+                            // Push the result onto the stack (or null if no results)
+                            $stack[] = $eaches[$index][2] ?: null;
+                            unset($eaches[$index]);
+                            $iter->seek($jmp - 1);
+                        }
 
-    /**
-     * Iterates over each element in the TOS.
-     *
-     * VM maintains an indexed array of eaches where each array contains an
-     * iterator, position to jump to when complete, and an array of results.
-     *
-     * 1. Check if an eaches hash exists at the given bytecode index:
-     * 1.a. If the array exists, append TOS to the array result if not empty.
-     *      Increment the iterator for the eaches array.
-     * 1.b. If the array does not exist and TOS is not an array, jump to the
-     *      given $arg in the bytecode.
-     * 1.c. If the array does not exist and TOS is an array, the VM creates a
-     *      new eaches entry, pops the TOS, and pushes the first key of the
-     *      array on TOS.
-     * 2.a. If the iterator is valid, push the current iterator element onto TOS
-     * 2.b. If the iterator is invalid, pop TOS, push the result onto TOS, and
-     *      jump to the jump position of the eaches hash.
-     *
-     * @param int $arg Jump to the given index when finished
-     */
-    private function op_each($arg)
-    {
-        $index = $this->i->key();
-        $tos = array_pop($this->stack);
+                    } elseif (is_array($tos)) {
+                        // It can be iterated so track the iteration at the current position
+                        $eachIter = new \ArrayIterator($tos);
+                        $eaches[$index] = [$eachIter, $arg, []];
+                        $stack[] = $eachIter->current();
+                    } else {
+                        // If it can't be iterated, jump right away
+                        $stack[] = $tos;
+                        $iter->seek($arg - 1);
+                    }
+                    break;
 
-        if (isset($this->eaches[$index])) {
+                case 'stop': break;
 
-            list($iter, $jmp,) = $this->eaches[$index];
-            // Add to the results if not empty
-            if (!empty($tos)) {
-                $this->eaches[$index][2][] = $tos;
+                default:
+                    throw new \RuntimeException('Unknown opcode {$op}');
+                    break;
             }
 
             $iter->next();
-            if ($iter->valid()) {
-                $this->stack[] = $iter->current();
-            } else {
-                // Push the result onto the stack (or null if no results)
-                $this->stack[] = $this->eaches[$index][2] ?: null;
-                unset($this->eaches[$index]);
-                $this->i->seek($jmp - 1);
-            }
-
-        } elseif (is_array($tos)) {
-            // It can be iterated so track the iteration at the current position
-            $iter = new \ArrayIterator($tos);
-            $this->eaches[$index] = [$iter, $arg, []];
-            $this->stack[] = $iter->current();
-        } else {
-            // If it can't be iterated, jump right away
-            $this->stack[] = $tos;
-            $this->i->seek($arg - 1);
         }
+
+        return array_pop($stack);
     }
 
     private function debugInit(array $opcodes, array $data)
@@ -284,16 +187,16 @@ class Interpreter
         echo "Execution stack\n===============\n\n";
     }
 
-    private function debugLine($op)
+    private function debugLine($key, $stack, $op)
     {
         $arg = 'op_' . $op[0];
-        $opLine = '> ' .    str_pad($this->i->key(), 3, ' ', STR_PAD_RIGHT) . ' ';
+        $opLine = '> ' .    str_pad($key, 3, ' ', STR_PAD_RIGHT) . ' ';
         $opLine .= str_pad($arg, 17, ' ') . '   ';
         $opLine .= str_pad((isset($op[1]) ? json_encode($op[1]) : null), 12, ' ');
         echo $opLine . "\n";
         echo str_repeat('-', strlen($opLine)) . "\n\n";
 
-        foreach (array_reverse($this->stack) as $index => $stack) {
+        foreach (array_reverse($stack) as $index => $stack) {
             echo '    ' . str_pad($index, 3, ' ', STR_PAD_LEFT) . ': ';
             echo json_encode($stack) . "\n";
         }
