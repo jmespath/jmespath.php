f2de96307c4e68c2181d2211e6d485a738b7aa02
Michael Dowling <mtdowling@gmail.com>
2013-11-29 13:32:04 -0800

    Changing how concat works

--- src/JmesPath/Fn/FnConcat.php
+++ src/JmesPath/Fn/FnConcat.php
@@ -3,26 +3,27 @@
 namespace JmesPath\Fn;
 
 /**
- * This method accepts a variable number of arguments, and concatenates each
- * argument. Each argument MUST be a String. If anything other than a String
- * is passed in any argument, this function MUST return null.
+ * Returns each argument concatenated one after the other. Any argument that
+ * is not a String or Number is excluded from the concatenated result. If no
+ * arguments are Strings or Numbers, this method MUST return null.
  */
 class FnConcat extends AbstractFn
 {
     protected $rules = array(
-        'arity' => array(2, -1),
-        'args' => array(
-            'default' => array('type' => 'string', 'failure' => 'null')
-        )
+        'arity' => array(2, -1)
     );
 
     protected function execute(array $args)
     {
+        $found = 0;
         $result = '';
         foreach ($args as $arg) {
-            $result .= $arg;
+            if (is_string($arg) || is_integer($arg)) {
+                $result .= $arg;
+                $found++;
+            }
         }
 
-        return $result;
+        return $found ? $result : null;
     }
 }
