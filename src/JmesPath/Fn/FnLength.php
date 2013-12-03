5f2cb0af3213866ebfd317dfd8211185bf3adc06
Michael Dowling <mtdowling@gmail.com>
2013-12-02 21:46:13 -0800

    Updating length to handle arrays, objects, and strings

--- src/JmesPath/Fn/FnLength.php
+++ src/JmesPath/Fn/FnLength.php
@@ -5,21 +5,24 @@ namespace JmesPath\Fn;
 /**
  * String|null length(String $subject)
  *
- * Returns the length of the String passed in the $subject argument.
+ * Returns the length of the given argument using the following types:
  *
- * If $subject is not a String, this function MUST return null.
+ * 1. String: returns the number of characters in the String
+ * 2. Array: returns the number of elements in the Array
+ * 3. Object: returns the number of key-value pairs in the Object
+ * 4. Boolean, null: returns null
  */
 class FnLength extends AbstractFn
 {
     protected $rules = array(
         'arity' => array(1, 1),
         'args'  => array(
-            0 => array('type' => 'string', 'failure' => 'null')
+            0 => array('type' => array('string', 'array'), 'failure' => 'null')
         )
     );
 
     protected function execute(array $args)
     {
-        return strlen($args[0]);
+        return is_array($args[0]) ? count($args[0]) : strlen($args[0]);
     }
 }
